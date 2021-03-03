146b211e0ede875d2fb4f3478afaf7be0c64d2d6
Alexandre Quevat <a.quevat@gmail.com>
2021-03-03 20:35:24 +0100

    home page with Boyfriends cards

--- app/controllers/boyfriends_controller.rb
+++ app/controllers/boyfriends_controller.rb
@@ -3,9 +3,9 @@ class BoyfriendsController < ApplicationController
 def search
 end
 
-  def index
-    @boyfriends = Boyfriend.all
-  end
+def index
+  @boyfriends = Boyfriend.all
+end
 
 def show
   @boyfriend = Boyfriend.find(params[:id])
