4f2d905a328c4d2809a062f6bf9ba004387d2c55
Alexandre Quevat <a.quevat@gmail.com>
2021-03-06 15:19:40 +0100

    adding create form

--- app/controllers/boyfriends_controller.rb
+++ app/controllers/boyfriends_controller.rb
@@ -1,4 +1,5 @@
 class BoyfriendsController < ApplicationController
+before_action :authenticate_user!
 
   def search
   @boyfriend = params[:search]
@@ -12,5 +13,25 @@ class BoyfriendsController < ApplicationController
 
   def show
     @boyfriend = Boyfriend.find(params[:id])
+  end
+
+  def new
+    @boyfriend = Boyfriend.new
+  end
+
+  def create
+    @boyfriend = Boyfriend.new(boyfriend_params)
+    # @boyfriend = BoyfriendService.find(params[:service_id])
+    @user = current_user
+    # @boyfriend.boyfriend_service = @boyfriend
+    @boyfriend.user = @user
+    @boyfriend.save
+    redirect_to new_boyfriend_path(@boyfriend)
+  end
+
+  private
+
+  def boyfriend_params
+    params.require(:boyfriend).permit(:name, :age, :ville, :description, :photos)
   end
 end
