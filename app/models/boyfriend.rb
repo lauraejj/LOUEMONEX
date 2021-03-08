2ef2035f6e7455805ac24e3ed09ab5f7a68eba03
laura julien <laura.julien@ymail.com>
2021-03-08 20:31:04 +0100

    added prices to boyfriends

--- app/models/boyfriend.rb
+++ app/models/boyfriend.rb
@@ -1,7 +1,10 @@
 class Boyfriend < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
-
+  validates :ville, presence: true
+  validates :age, presence: true
+  validates :description, presence: true
+  validates :name, presence: true, uniqueness: true
   has_many_attached :photos
   include PgSearch::Model
     pg_search_scope :search_by_name_and_ville,
