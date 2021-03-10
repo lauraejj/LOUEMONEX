2506e2e06f1046ca6ff612d2e6fdf32fd413ffd0
Alexandre Quevat <a.quevat@gmail.com>
2021-03-10 21:56:14 +0100

    correction de la map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,4 @@
 // @import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder';
-import { initMapbox } from '../plugins/init_mapbox';
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -11,6 +10,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
+initMapbox.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
