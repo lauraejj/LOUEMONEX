bbcd23588a54791cc74e5c44b3f29260950fc4ab
Irene Nabet <irene.nabet@gmail.com>
2021-03-13 15:20:01 +0100

    css

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,6 @@
 Unsplash.configure do |config|
-  config.application_access_key = "Ifk39ohhiEMTYwjn58stlbwXglwPqnt5b07MgY3MNcM"
-  config.application_secret = "HXcTmbWUXjbwZBAo5PX8aAD-z0CzRKFiifYG13JHusA"
+  config.application_access_key = ENV["UNSPLASH_API_KEY"]
+  config.application_secret = ENV["UNSPLASH_API_SECRET_KEY"]
   config.application_redirect_uri = "https://your-application.com/oauth/callback"
   config.utm_source = "alices_terrific_client_app"
 end
